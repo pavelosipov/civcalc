773d6e3033386e291ae355067bb8ee684510b239
Pavel Osipov <p.osipov@corp.mail.ru>
2013-12-11 13:26:43 +0400

    roadmap for all t55

--- CivCalc/main.cpp
+++ CivCalc/main.cpp
@@ -19,32 +19,22 @@
 static void processAmsterdamBuildingQueue(uint8_t startTurn) {
     std::cout << "AMSTERDAM" << std::endl;
     City city;
-    city.setPopulation(2);
+    city.setPopulation(3);
     city.setTiles({
         Tile::create(2, 1, 11), // city
         Tile::create(5, 0, 3),  // fish
         Tile::create(4, 2, 1),  // cow
-        Tile::create(1, 3, 1),  // riverside mine
-        Tile::create(2, 0, 1)   // riverside graasland
+        Tile::create(2, 0, 3),  // riverside graasland cottage
+        Tile::create(2, 0, 3),  // riverside graasland cottage
+        Tile::create(1, 3, 1)   // riverside mine
     });
-    city.setAccumulatedGoods(Goods(14, 0, 0));
-    city.pushBuilding(Building::worker(35));
-    city.pushBuilding(Building::workBoat());
-    city.pushBuilding(Building::settler());
-    city.pushBuilding(Building::worker());
+    city.setAccumulatedGoods(Goods(4, 0, 0));
+    city.pushBuilding(Building::settler(84));
+    city.pushBuilding(Building::workBoat(11));
+    city.pushBuilding(Building::granary());
+    city.pushBuilding(Building::axeman());
     ActionQueue actionQueue;
-    actionQueue.pushAction(startTurn + 5, AnyAction::create([](City &city, Goods &turnGoods) {
-        city.swapBuildings(0, 1);
-    }));
-    actionQueue.pushAction(startTurn + 8, AnyAction::create([](City &city, Goods &turnGoods) {
-        city.chop();
-    }));
-    actionQueue.pushAction(startTurn + 13, AnyAction::create([](City &city, Goods &turnGoods) {
-        city.tileAt(4)->setGoods(Goods(2, 0, 3)); // graasland improved
-        city.turnLogger().addEvent("COTTAGE COMPLETED");
-        city.swapTiles(3, 4);
-    }));
-//    actionQueue.pushAction(13, WhipAction::create());
+    actionQueue.pushAction(startTurn + 10, WhipAction::create());
     city.processBuildingQueue(startTurn, actionQueue);
 }
 
@@ -54,23 +44,24 @@ static void processUtrechtBuildingQueue(uint8_t startTurn) {
     city.setTiles({
         Tile::create(2, 1, 2), // city
         Tile::create(4, 2, 0), // cows
+        Tile::create(5, 1, 1), // wheat
+        Tile::create(2, 0, 3), // improved cottage
+        Tile::create(2, 0, 0), // cottage
         Tile::create(1, 3, 1), // riverside mine
-        Tile::create(2, 0, 3), // cottage
-        Tile::create(2, 0, 3), // cottage
-        Tile::create(5, 1, 1)  // wheat
     });
     city.setPopulation(2);
-    city.setAccumulatedGoods(Goods(9, 0, 0));
-    city.pushBuilding(Building::warrior());
+    city.setAccumulatedGoods(Goods(15, 0, 0));
+    city.pushBuilding(Building::warrior(12));
     city.pushBuilding(Building::granary(4));
     ActionQueue actionQueue;
-    actionQueue.pushAction(startTurn + 2, AnyAction::create([](City &city, Goods &turnGoods) {
-        city.swapTiles(2, 5);
+    actionQueue.pushAction(startTurn + 3, AnyAction::create([](City &city, Goods &turnGoods) {
+        city.tileAt(4)->setGoods(Goods(2, 0, 3));
+        city.turnLogger().addEvent("COTTAGE IMPROVED");
     }));
-    actionQueue.pushAction(startTurn + 7, AnyAction::create([](City &city, Goods &turnGoods) {
+    actionQueue.pushAction(startTurn + 5, AnyAction::create([](City &city, Goods &turnGoods) {
         city.setTopBuilding(Building::settler());
     }));
-    actionQueue.pushAction(startTurn + 13, WhipAction::create());
+    actionQueue.pushAction(startTurn + 11, WhipAction::create());
     city.processBuildingQueue(startTurn, actionQueue);
 }
 
@@ -82,31 +73,28 @@ static void processTheHagueBuildingQueue(uint8_t startTurn) {
     city.setTiles({
         Tile::create(2, 1, 2), // city
         Tile::create(4, 0, 3), // clam
+        Tile::create(5, 0, 3), // fish
         Tile::create(1, 5, 0), // copper
-        Tile::create(2, 0, 3), // fish
         Tile::create(1, 1, 0)  // mine
     });
-    city.pushBuilding(Building::galley(28));
-    city.pushBuilding(Building::workBoat());
+    city.pushBuilding(Building::granary(7));
     city.pushBuilding(Building::barracks());
-    city.pushBuilding(Building::axeman());
     ActionQueue actionQueue;
-    actionQueue.pushAction(startTurn + 8, WhipAction::create());
-    actionQueue.pushAction(startTurn + 10, AnyAction::create([](City &city, Goods &turnGoods) {
-        city.tileAt(3)->setGoods(Goods(5, 0, 3)); // fish improved
-        city.swapTiles(2, 3);
+    actionQueue.pushAction(startTurn + 1, AnyAction::create([](City &city, Goods &turnGoods) {
+        city.swapTiles(1, 2);
     }));
-    actionQueue.pushAction(startTurn + 14, AnyAction::create([](City &city, Goods &turnGoods) {
+    actionQueue.pushAction(startTurn + 7, AnyAction::create([](City &city, Goods &turnGoods) {
         city.setTopBuilding(Building::settler());
     }));
-    actionQueue.pushAction(startTurn + 27, WhipAction::create());
+    actionQueue.pushAction(startTurn + 13, WhipAction::create());
+    actionQueue.pushAction(startTurn + 21, WhipAction::create());
     city.processBuildingQueue(startTurn, actionQueue);
 }
 
 int main(int argc, const char * argv[]) {
-    const uint8_t startTurn = 53;
-//    processAmsterdamBuildingQueue(startTurn);
+    const uint8_t startTurn = 55;
+    processAmsterdamBuildingQueue(startTurn);
     processUtrechtBuildingQueue(startTurn);
-//    processTheHagueBuildingQueue(startTurn);
+    processTheHagueBuildingQueue(startTurn);
     return 0;
 }
